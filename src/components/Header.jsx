bdea0829bf78dea4c00f4bf10ad9277f04cf93db
omarosvaldogm <omarogomezm@gmail.com>
2025-08-20 12:17:23 -0600

    se arreglaron las rutas de los botones de inicio de sesion

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -164,13 +164,13 @@ const Header = () => {
                     Plataforma de rastreo
                   </button>
                   <button 
-                    onClick={() => redirectToExternalPage('https://ananta.com.mx')}
+                    onClick={() => redirectToExternalPage('https://www.ananta.com.mx')}
                     className="block w-full text-left px-4 py-2 text-gray-300 hover:text-white hover:bg-white/10 transition-colors duration-300"
                   >
                     Ananta
                   </button>
                   <button 
-                    onClick={() => redirectToExternalPage('https://varuna.com.mx')}
+                    onClick={() => redirectToExternalPage('https://www.varuna.com.mx')}
                     className="block w-full text-left px-4 py-2 text-gray-300 hover:text-white hover:bg-white/10 transition-colors duration-300"
                   >
                     Varuna
@@ -289,7 +289,7 @@ const Header = () => {
                     Plataforma de rastreo
                   </button>
                   <button 
-                    onClick={() => redirectToExternalPage('https://wwww.ananta.com.mx')}
+                    onClick={() => redirectToExternalPage('https://www.ananta.com.mx')}
                     className="block w-full text-left text-gray-400 hover:text-white transition-colors duration-300 py-1"
                   >
                     Ananta
